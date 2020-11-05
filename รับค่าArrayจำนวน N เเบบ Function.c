cac2ebd85c404bb40733784da9d1b840ec094289
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2020-11-05 23:09:56 +0700

    รับค่าArrayจำนวนNตัวเเบบFunction

---
+++ รับค่าArrayจำนวน N เเบบ Function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+demo(int *a);
+    for(int i =0;i<10;i++){
+        printf("%d ",a[i]);
+    }
+
+int main()
+{
+    int n;
+
+    printf("Input Arry[n] : ");
+    scanf("%d",&n);
+    int *arr[n];
+    for(int i = 1; i<=n; i++)
+    {
+        printf("n[%d]: ",i);
+        scanf(" %d",&arr[i]);
+
+    }
+    demo(arr);
+
+    return 0;
+
+
+}
